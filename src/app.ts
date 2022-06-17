cc01d0a069f6ab4d9d1b1bd47c5afda1503bd93c
Titancube <kbin0717@gmail.com>
2022-06-17 17:47:49 +0900

    test: ts-node

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,17 @@
 import express, { Request, Response, NextFunction } from "express"
 import { Client } from "pg";
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 const app = express();
 
 const client = new Client()
 
-app.get('/test', (request, response) => {
-  // client.connect()
-  // client.query('SELECT firstname from contact', (err, res) => {
-  //   console.log(err ? err.stack : response.send(res.rows[0]))
-  //   client.end()
-  // })
-
-  response.json({
-    message: "hello"
+app.get('/test', (request: Request, response: Response, next: NextFunction) => {
+  client.connect()
+  client.query('SELECT firstname from contact', (err, res) => {
+    err ? console.log(err.stack) : response.send(res.rows[0])
+    client.end()
   })
 })
 
