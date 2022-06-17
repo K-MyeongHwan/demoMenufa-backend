cf2bf2e5936472ff07ff59082b6d993d2ad56b35
Titancube <kbin0717@gmail.com>
2022-06-17 16:04:09 +0900

    add: default return

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ const app = express();
 const client = new Client()
 await client.connect()
 
-client.query('SELECT firstname from contact', (err, res) => {
-  console.log(err ? err.stack : res.rows[0])
-  client.end()
+app.get('/', (request, response) => {
+  client.query('SELECT firstname from contact', (err, res) => {
+    console.log(err ? err.stack : response.send(res.rows[0]))
+    client.end()
+  })
 })
