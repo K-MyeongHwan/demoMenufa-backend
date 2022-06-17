425b30e99783c4abe9e82f8514203f945150862f
Titancube <kbin0717@gmail.com>
2022-06-17 17:05:34 +0900

    fix: esm import

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,15 @@ import { Client } from "pg";
 const app = express();
 
 const client = new Client()
-await client.connect()
 
 app.get('/', (request, response) => {
+  client.connect()
   client.query('SELECT firstname from contact', (err, res) => {
     console.log(err ? err.stack : response.send(res.rows[0]))
     client.end()
   })
+})
+
+app.listen(3000, () => {
+  console.log('server started')
 })
