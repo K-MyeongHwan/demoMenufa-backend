34bbc8d7beaa1ab736fdd73e040d54035ec2bfdf
Gyubin Lee <kbin0717@gmail.com>
2022-06-27 16:30:17 +0900

    add: add query

--- src/controllers/area.ts
+++ src/controllers/area.ts
@@ -6,28 +6,35 @@ import { Request, Response } from "express";
 import { db } from "../db";
 
 export const area = async (req: Request, res: Response) => {
-  const client = await db.connect();
-  client.on("error", (err) => {
-    console.log(err);
-  });
-  const result = await client.query(
-    "select id, name from salesforce.area__c order by id asc"
-  );
-  res.json(result.rows);
-  client.release();
-};
-
-export const modifyArea = async (req: Request, res: Response) => {
-  const { id, name } = req.query;
-  const client = await db.connect();
-
   try {
+    const client = await db.connect();
     const result = await client.query(
-      `update salesforce.area__c set name = '${name}' where id = '${id}'`
+      "select id, name from salesforce.area__c order by id asc"
     );
+    res.json(result.rows);
     client.release();
-    res.json(result);
   } catch (error) {
     res.send(["Something went wrong", error]);
   }
+};
+
+export const areaManipulator = async (req: Request, res: Response) => {
+  const { id, name } = req.query;
+
+  try {
+    const client = await db.connect();
+    const result = await client.query(id ? update(id, name) : add(name));
+    client.release();
+    res.status(200);
+  } catch (error) {
+    res.send(["Something went wrong", error]);
+  }
+};
+
+const update = (id: any, name: any) => {
+  return `update salesforce.area__c set name = '${name}' where id = '${id}'`;
+};
+
+const add = (name: any) => {
+  return `insert into salesforce.area__c (name) values ('${name}')`;
 };
