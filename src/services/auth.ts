c14396946ec6cb0c413ee8b8b581d01cdaa37550
Gyubin Lee <kbin0717@gmail.com>
2022-06-28 17:19:38 +0900

    add: password serialization

---
+++ src/services/auth.ts
@@ -0,0 +1,30 @@
+import util from "util";
+import crypto from "crypto";
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+const pbkdf2Promise = util.promisify(crypto.pbkdf2);
+
+const createSalt = async () => {
+  const buf = await randomBytesPromise(64);
+  return buf.toString("base64");
+};
+
+export const serializePassword = async (password: string) => {
+  // if (!password) return;
+  const salt = await createSalt();
+  const key = await pbkdf2Promise(password, salt, 104906, 64, "sha512");
+  const hashedPassword = key.toString("base64");
+
+  return { hashedPassword, salt };
+};
+
+export const checkPassword = async (
+  password: string,
+  salt: string,
+  userInput: string
+) => {
+  const key = await pbkdf2Promise(userInput, salt, 104906, 64, "sha512");
+  const hashedPassword = key.toString("base64");
+  console.log(hashedPassword, password);
+  return password === hashedPassword;
+};
