2f2daf3e6f80f1863fcc504d7bfe002d4f12bd5e
Gyubin Lee <kbin0717@gmail.com>
2022-07-04 15:32:49 +0900

    add: logistics list by area

--- src/routes/logisticsRouter.ts
+++ src/routes/logisticsRouter.ts
@@ -1,9 +1,14 @@
 import express, { Router } from "express";
-import { createLogistics, logisticsList } from "../controllers/logistics";
+import {
+  createLogistics,
+  logisticsList,
+  logisticsListByArea,
+} from "../controllers/logistics";
 
 const router: Router = express.Router();
 
 router.get("/", logisticsList);
+router.get("/logisticsCenter", logisticsListByArea);
 router.post("/", createLogistics);
 
 export default router;
